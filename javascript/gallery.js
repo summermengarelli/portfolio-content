e2f1b23dad900c86133e077f4f7c2335083fa942
summermengarelli <89875738+summermengarelli@users.noreply.github.com>
2022-12-05 18:10:30 -0500

    Add files via upload

---
+++ javascript/gallery.js
@@ -0,0 +1,8 @@
+//play bird songs on Gallery page load
+
+
+    let galSound = document.getElementById('gal-sound');
+
+    function playSound() {
+        galSound.play(); //play the audio file
+     };
